3c1db59a8135763fba5cae72fd37459f9211531f
daniela <daniela.zolnowska@gmail.com>
2019-11-24 21:53:38 +0100

    LAB4 Configure DB, add jmeter tests

--- src/main/java/pl/edu/pjatk/tau/bookstore/domain/Book.java
+++ src/main/java/pl/edu/pjatk/tau/bookstore/domain/Book.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Entity
@@ -17,6 +14,7 @@ import javax.persistence.Id;
 public class Book {
 
 	@Id
+	@Column(name="bookId")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Setter
 	private Long id;
